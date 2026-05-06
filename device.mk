b699aaed779c73fa3652aad0812a9fcdf928eac2
amit-0i <amit0i+github@proton.me>
2026-05-07 01:42:44 +0530

    ziti: build IR Blaster
    Signed-off-by: amit-0i <amit0i+github@proton.me>

--- device.mk
+++ device.mk
@@ -35,6 +35,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+$(call inherit-product-if-exists, vendor/oneplus/ir/config.mk)
+
 PRODUCT_PACKAGES += \
     android.hardware.ir-service.oplus
 
