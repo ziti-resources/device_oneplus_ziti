abd8245b5c70f22e6fc231bb342842a8fd21a8a6
amit-0i <amit0i+github@proton.me>
2026-05-14 12:18:24 +0530

    ziti: lineage -> voltage
    Signed-off-by: amit-0i <amit0i+github@proton.me>

--- device.mk
+++ device.mk
@@ -59,7 +59,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-voltage
 
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
--- voltage_ziti.mk
+++ voltage_ziti.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from ziti device
 $(call inherit-product, device/oneplus/ziti/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Voltage stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_ziti
+PRODUCT_NAME := voltage_ziti
 PRODUCT_DEVICE := ziti
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
