8151dc69291609a270d41ca489e553e129c003f9
amit-0i <amit0i+github@proton.me>
2026-05-30 19:13:16 +0530

    ziti: build GameBar
    Signed-off-by: amit-0i <amit0i@proton.me>

--- device.mk
+++ device.mk
@@ -31,6 +31,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     debloatPackage
 
+# GameBar
+$(call inherit-product-if-exists, packages/apps/GameBar/gamebar.mk)
+
 # IR Blaster
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
