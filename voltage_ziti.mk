db3f6d3deded9c79d3bf45f2bef4dda353aed73f
amit-0i <amit0i@proton.me>
2026-06-25 16:39:32 +0530

    ziti: bringup VoltageOS
    Signed-off-by: amit-0i <amit0i@proton.me>

--- voltage_ziti.mk
+++ voltage_ziti.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from ziti device
 $(call inherit-product, device/oneplus/ziti/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Voltage stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_ziti
+PRODUCT_NAME := voltage_ziti
 PRODUCT_DEVICE := ziti
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
@@ -30,3 +30,14 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=CPH2569 \
     SystemDevice=OP5953L1 \
     SystemName=CPH2569
+
+# Device attestation
+PRODUCT_MANUFACTURER_FOR_ATTESTATION := OnePlus
+PRODUCT_BRAND_FOR_ATTESTATION :=  OnePlus
+PRODUCT_DEVICE_FOR_ATTESTATION :=  ziti
+PRODUCT_NAME_FOR_ATTESTATION := OnePlus Nord CE3 5G
+PRODUCT_MODEL_FOR_ATTESTATION := CPH2569
+
+# VoltageOS flags
+PRODUCT_NO_CAMERA:= true
+EXTRA_UDFPS_ANIMATIONS:= true
